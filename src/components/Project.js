425a90ac9b7e7396c24a9455a5a0857e31841aa2
muskan2566 <muskankushwah2566@gmail.com>
2021-10-01 21:34:49 +0530

    project page added

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import './css/project.css'
+import P1 from './image/p1.png'
 import { Link } from 'react-router-dom'
 
 export default function Project() {
     return (
         <>
-           <h1>my projects</h1> 
-           <Link to="/">HOME</Link>
+            <div className="project-Navbar ">
+                <div>
+                    <Link className="project-link" to="/">HOME</Link>
+                    <Link className="project-link" to="/Project">PROJECTS</Link>
+                </div>
+            </div>
+            <div class="project-title">
+                <p className="projecth"><b>My Projects</b></p>
+               
+
+                <hr/>
+                <div className="project-body" >
+                    <div>
+                        <img className="project-image" src={P1} alt="not found" />
+                    </div>
+                    <div className="project-details">
+                        <p className="project-h">College Essential</p>
+                        <p className="project-tech"><i>Technology (HTML,CSS,Javascript)</i></p>
+                        <p className="project-info">This is a Website for MCA,BCA and DDMCA Students.
+                            You can download Books, Syllabus(RGPV), Notes, Exam-papers from here.</p>
+                          
+                            <button className="P-button" onclick="https://vm-collagewebsite.netlify.app/">Git Hub</button>
+                            <button className="P-button" onclick="https://vm-collagewebsite.netlify.app/">Live Demo</button>
+                        
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
