0706268c53303ff6332145b43cf4b66913f93662
Muskan Kushwah <83701864+muskan2566@users.noreply.github.com>
2022-12-11 14:33:48 +0530

    new project added

---
+++ src/components/projectData.js
@@ -0,0 +1,45 @@
+
+import P2 from './image/p2.png'
+import P1 from './image/p1.png'
+import P3 from  './image/p3.png'
+const projectData = [
+    {
+        image:P1,
+        name : "CryptoDash",
+        tech: "(Reactjs,CSS)",
+        detail: "CryptoDash is a web application that provides an interface to see all the current prices of Crypto coins.",
+        git:"https://github.com/muskan2566/Crypto-Dash",
+        live:"https://cryptodashb.netlify.app",
+
+    },
+    {
+        image:P2,
+        name : "CollegeEssential",
+        tech: "(HTML,CSS,Javascript)",
+        detail: "This is a Website for MCA,BCA and DDMCA Students.You can download Books, Syllabus(RGPV), Notes, Exam-papers from here.",
+        git:"https://github.com/muskan2566/Collage-Essentials/",
+        live:"https://vm-collagewebsite.netlify.app/",
+
+    },
+    {
+        image:P3,
+        name : "ImageGallery",
+        tech: "(MERN)",
+        detail: "This is a web app where you can add your images in DataBase. Also perform CURD operations like you can add ,view , change images in database.",
+        git:"https://github.com/muskan2566/ImageGalleryClint",
+        live:"https://imagegalleryhub.netlify.app",
+
+    },
+    {
+        image:P4,
+        name : "SuperResolution",
+        tech: "(React.js,CSS,FastAPI,ML)",
+        detail: "This is a web application helps you to convert low resolution images to high resoluiton images ",
+        git:"https://github.com/muskan2566/Super-res",
+        live:"",
+
+    }
+    
+]
+
+export default projectData;
